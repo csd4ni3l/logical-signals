2fde952e58bca6f823f3c0114835386765b4780a
csd4ni3l <v3xq21w7@duck.com>
2025-10-13 11:07:58 +0200

    First commit from game template

---
+++ game/play.py
@@ -0,0 +1,11 @@
+import arcade, arcade.gui
+
+from utils.constants import menu_background_color, button_style
+from utils.preload import button_texture, button_hovered_texture
+
+class Game(arcade.gui.UIView):
+    def __init__(self, pypresence_client):
+        super().__init__()
+
+    def on_show_view(self):
+        super().on_show_view()
